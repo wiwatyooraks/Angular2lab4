f4d934ddbe4ab61bbf10e70543baef24f1e9ad96
wiwatyooraks <78940910+wiwatyooraks@users.noreply.github.com>
2021-04-04 17:10:23 +0700

    Angular2lab4

---
+++ app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component , ViewChild , OnInit} from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'learn-angular';
+   
+  @ViewChild('productList', { static:true })
+  productList: ProductListComponent;
+
+  ngOnInit() : void {
+    this.productList.products = [
+      {
+      name: 'ส้มโอ',
+      price: 111
+      },
+      {
+      name: 'แตงโม',
+      price: 222
+      },
+      {
+      name: 'มะพร้าวนํ้าหอม',
+      price: 333
+      },
+      ];
+  }
+}
